01235c15292c43d1764ec61bb2032e9f06914327
abhishekmahajan <abhishekmahajan8285@gmail.com>
2025-04-04 23:25:02 +0530

    pr

--- src/pages/adduser.jsx
+++ src/pages/adduser.jsx
@@ -20,38 +20,7 @@ import {
 const UserManagementPage = () => {
   const [showPopup, setShowPopup] = useState(false);
   const [allUsers, setAllUsers] = useState([]);
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      name: "Simran",
-      email: "simran@gmail.com",
-      Department: "Admin",
-      Designation: "Project Manager",
-      project: "CRM Dashboard",
-      status: "Active",
-      profileImage: girlpicture,
-    },
-    {
-      id: 2,
-      name: "Tanu",
-      email: "tanu@gmail.com",
-      Department: "Team Member",
-      Designation: "Frontend Developer",
-      project: "E-Commerce Platform",
-      status: "Pending",
-      profileImage: girlpicture,
-    },
-    {
-      id: 3,
-      name: "Anshu",
-      email: "anshu@gmail.com",
-      Department: "Intern",
-      Designation: "Backend Developer",
-      project: "Mobile App",
-      status: "Invited",
-      profileImage: girlpicture,
-    },
-  ]);
+  const [users, setUsers] = useState();
 
   //api integration
   const fetchData = async () => {
@@ -89,80 +58,70 @@ const UserManagementPage = () => {
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header />
-        <div className="p-6 w-full bg-gray-100 min-h-screen">
-          <div className="mb-4">
-            <button
-              className="bg-blue-600  rounded shadow p-4 hover:bg-blue-800 text-white font-bold py-2 px-4 "
-              onClick={openPopup}
-            >
-              <MdPersonAdd className="inline-block mr-2" /> Add Team Member
-            </button>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="text-center">Profile</TableHead>
-                  <TableHead className="text-center">Name</TableHead>
-                  <TableHead className="text-center">Email</TableHead>
-                  <TableHead className="text-center">Department</TableHead>
-                  <TableHead className="text-center">Designation</TableHead>
-                  <TableHead className="text-center">
-                    Assigned Project
-                  </TableHead>
-                  <TableHead className="text-center">Permission</TableHead>
-                  <TableHead className="text-center">Post Module</TableHead>
-                  <TableHead className="text-center">User Type</TableHead>
-                  <TableHead className="text-center">Status</TableHead>
-                  <TableHead className="text-center">Action</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {allUsers.map((row, index) => (
-                  <TableRow key={row._id}>
-                    <TableCell>
-                      <img
-                        src={row.profileImage || Profile}
-                        alt="Profile"
-                        className="w-10 h-10 rounded-full object-cover"
-                      />
-                    </TableCell>
-                    <TableCell>{row.name}</TableCell>
-                    <TableCell>{row.email}</TableCell>
-                    <TableCell>{row.department || "N/A"}</TableCell>
-                    <TableCell>{row.designation || "N/A"}</TableCell>
-                    <TableCell>{row.project || "N/A"}</TableCell>
-                    <TableCell>
-                      {row.permission?.join(", ") || "None"}
-                    </TableCell>
-                    <TableCell>
-                      {row.postModule?.join(", ") || "None"}
-                    </TableCell>
-                    <TableCell>{row.userType || "N/A"}</TableCell>
-                    <TableCell
-                      className={`p-4 font-bold ${
-                        row.status?.toLowerCase() === "active"
-                          ? "text-green-600"
-                          : "text-yellow-600"
-                      }`}
-                    >
-                      {row.status || "Inactive"}
-                    </TableCell>
-                    <TableCell
-                      className="text-blue-500 hover:text-blue-700 text-xl pr-6"
+        <div className="p-6 w-full mb-4 bg-gray-100 min-h-screen">
+          <button
+            className="bg-blue-600  rounded shadow p-4 hover:bg-blue-800 text-white font-bold py-2 px-4 "
+            onClick={openPopup}
+          >
+            <MdPersonAdd className="inline-block mr-2" /> Add Team Member
+          </button>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="text-center">Profile</TableHead>
+                <TableHead className="text-center">Name</TableHead>
+                <TableHead className="text-center">Email</TableHead>
+                <TableHead className="text-center">Department</TableHead>
+                <TableHead className="text-center">Designation</TableHead>
+                <TableHead className="text-center">Assigned Project</TableHead>
+                <TableHead className="text-center">Permission</TableHead>
+                <TableHead className="text-center">Post Module</TableHead>
+                <TableHead className="text-center">User Type</TableHead>
+                <TableHead className="text-center">Status</TableHead>
+                <TableHead className="text-center">Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {allUsers.map((row, index) => (
+                <TableRow key={row._id}>
+                  <TableCell>
+                    <img
+                      src={row.profileImage || Profile}
+                      alt="Profile"
+                      className="w-10 h-10 rounded-full object-cover"
+                    />
+                  </TableCell>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{row.department || "N/A"}</TableCell>
+                  <TableCell>{row.designation || "N/A"}</TableCell>
+                  <TableCell>{row.project || "N/A"}</TableCell>
+                  <TableCell>{row.permission?.join(", ") || "None"}</TableCell>
+                  <TableCell>{row.postModule?.join(", ") || "None"}</TableCell>
+                  <TableCell>{row.userType || "N/A"}</TableCell>
+                  <TableCell
+                    className={`p-4 font-bold ${
+                      row.status?.toLowerCase() === "active"
+                        ? "text-green-600"
+                        : "text-yellow-600"
+                    }`}
+                  >
+                    {row.status || "Inactive"}
+                  </TableCell>
+                  <TableCell className=" flex gap-2 text-xl">
+                    <LiaUserEditSolid
                       onClick={() => alert("Edit User details")}
-                    >
-                      <LiaUserEditSolid />
-                    </TableCell>
-                    <TableCell
-                      className="text-red-500 hover:text-red-700 text-xl"
+                      className="hover:text-blue-700  text-blue-500"
+                    />
+                    <MdDelete
                       onClick={() => handleDelete(row._id)}
-                    >
-                      <MdDelete />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
+                      className="text-red-500 hover:text-red-700"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
       {showPopup && <AddUserForm closePopup={closePopup} addUser={addUser} />}
